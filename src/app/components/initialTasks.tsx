4593cb8c48f489c3b959e8ba7d0c638a64dabaed
Crystal Parker <crystal@koii.network>
2024-11-03 17:12:53 -0400

    ensuring update happends on pwa on my phone too

--- src/app/components/initialTasks.tsx
+++ src/app/components/initialTasks.tsx
@@ -4,13 +4,13 @@ import { Task } from "./types";
 
 export const initialTasks: Task[] = [
     { id: 1, name: "Do the Dishes", points: 10, streak: 0, lastCompletedDate: null, completedToday: false, time: 900 },
-    { id: 2, name: "Study for 10 Minutes", points: 15, streak: 0, lastCompletedDate: null, completedToday: false, time: 600 },
-    { id: 3, name: "Tidy One Area", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 600 },
-    { id: 4, name: "Wipe Down Counters", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 300 },
-    { id: 5, name: "Make the Bed", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 180 },
-    { id: 6, name: "Prepare Tomorrow's Lunch", points: 10, streak: 0, lastCompletedDate: null, completedToday: false, time: 900 },
-    { id: 7, name: "Prepare Supper", points: 10, streak: 0, lastCompletedDate: null, completedToday: false, time: 1200 },
-    { id: 8, name: "Call a Friend or Family Member", points: 15, streak: 0, lastCompletedDate: null, completedToday: false, time: 600 },
-    { id: 9, name: "Brush your teeth", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 300 },
-    { id: 10, name: "Wipe Down Bathroom Sink", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 300 }
+    // { id: 2, name: "Study for 10 Minutes", points: 15, streak: 0, lastCompletedDate: null, completedToday: false, time: 600 },
+    // { id: 3, name: "Tidy One Area", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 600 },
+    // { id: 4, name: "Wipe Down Counters", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 300 },
+    // { id: 5, name: "Make the Bed", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 180 },
+    // { id: 6, name: "Prepare Tomorrow's Lunch", points: 10, streak: 0, lastCompletedDate: null, completedToday: false, time: 900 },
+    // { id: 7, name: "Prepare Supper", points: 10, streak: 0, lastCompletedDate: null, completedToday: false, time: 1200 },
+    // { id: 8, name: "Call a Friend or Family Member", points: 15, streak: 0, lastCompletedDate: null, completedToday: false, time: 600 },
+    // { id: 9, name: "Brush your teeth", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 300 },
+    // { id: 10, name: "Wipe Down Bathroom Sink", points: 5, streak: 0, lastCompletedDate: null, completedToday: false, time: 300 }
 ];
